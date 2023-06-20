6288fa1347dbfc2e25858735438e0e58c97cd50f
uconcreative <mail@uconcreative.com>
2023-06-20 14:55:47 +0900

    06/20

--- gameScreen.py
+++ gameScreen.py
@@ -1,87 +1,77 @@
-from tkinter import Widget
-
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-
-
-Builder.load_file('game.kv')
-
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.properties import NumericProperty, ObjectProperty, ReferenceListProperty
+from kivy.vector import Vector
+from kivy.core.window import Window
 
 class GameScreen(Screen):
     def __init__(self, **kwargs):
         super(GameScreen, self).__init__(**kwargs)
 
-    class PongPaddle(Widget):
-        score = NumericProperty(0)
+class PongPaddle(Widget):
+    score = NumericProperty(0)
 
-        def bounce_ball(self, ball):
-            if self.collide_widget(ball):
-                vx, vy = ball.velocity
-                offset = (ball.center_y - self.center_y) / (self.height / 2)
-                bounced = Vector(-1 * vx, vy)
-                vel = bounced * 1.1
-                ball.velocity = vel.x, vel.y + offset
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
 
-    class PongBall(Widget):
-        velocity_x = NumericProperty(0)
-        velocity_y = NumericProperty(0)
-        velocity = ReferenceListProperty(velocity_x, velocity_y)
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-        def move(self):
-            self.pos = Vector(*self.velocity) + self.pos
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
-    class PongGame(Widget):
-        ball = ObjectProperty(None)
-        player1 = ObjectProperty(None)
-        player2 = ObjectProperty(None)
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-        def __init__(self, PongGame=None, **kwargs):
-            super(PongGame, self).__init__(**kwargs)
-            from kivy.core.window import Window
-            self.keyboard = Window.request_keyboard(self._keyboard_closed, self)
-            self.keyboard.bind(on_key_down=self._on_keyboard_down)
+    def __init__(self, **kwargs):
+        super(PongGame, self).__init__(**kwargs)
+        self.keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self.keyboard.bind(on_key_down=self._on_keyboard_down)
 
-        def serve_ball(self, vel=(4, 0)):
-            self.ball.center = self.center
-            self.ball.velocity = vel
+    def serve_ball(self, vel=(4, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
 
-        def update(self, dt):
-            self.ball.move()
+    def update(self, dt):
+        self.ball.move()
 
-            # Bounce off paddles
-            self.player1.bounce_ball(self.ball)
-            self.player2.bounce_ball(self.ball)
+        # Bounce off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
-            # Bounce off top and bottom
-            if (self.ball.y < 0) or (self.ball.top > self.height):
-                self.ball.velocity_y *= -1
+        # Bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
 
-            # Score points
-            if self.ball.x < self.x:
-                self.player2.score += 1
-                self.serve_ball(vel=(4, 0))
-            if self.ball.x > self.width:
-                self.player1.score += 1
-                self.serve_ball(vel=(-4, 0))
+        # Score points
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
 
-        def _keyboard_closed(self):
-            self.keyboard.unbind(on_key_down=self._on_keyboard_down)
-            self.keyboard = None
+    def _keyboard_closed(self):
+        self.keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self.keyboard = None
 
-        def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-            if keycode[1] == 'w':
-                self.player1.center_y += 20
-            elif keycode[1] == 's':
-                self.player1.center_y -= 20
-            elif keycode[1] == 'up':
-                self.player2.center_y += 20
-            elif keycode[1] == 'down':
-                self.player2.center_y -= 20
-            return True
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'w':
+            self.player1.center_y += 20
+        elif keycode[1] == 's':
+            self.player1.center_y -= 20
+        elif keycode[1] == 'up':
+            self.player2.center_y += 20
+        elif keycode[1] == 'down':
+            self.player2.center_y -= 20
+        return True
